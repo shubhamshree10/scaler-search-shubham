08a7edab2508fedb2407c4634f545ab111347556
Shubham Shree <shubhamshree10@gmail.com>
2021-05-01 11:12:04 +0530

    Added styles

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <h1>Scaler Challenge</h1>
       <hr></hr>
+      <form>
+      <label for="search"><b>Search Name</b></label>
+       <input type="text" id="search" name="searchNames" placeholder="Type the name"></input>
+      </form>
     </div>
   );
 }
